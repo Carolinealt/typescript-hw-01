df47c2fed3d59c883062e06c51b8e7f8e835dda9
Your Name <e.scaroline02@gmail.com>
2024-07-13 19:52:21 +0300

    basics

---
+++ src/basic/6.ts
@@ -0,0 +1,21 @@
+interface User {
+    name: string,
+    age: number,
+    email: string,
+    address?: {},
+}
+const poly: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com'
+};
+
+const mango: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+        city: 'New York',
+        country: 'USA'
+    }
+};
