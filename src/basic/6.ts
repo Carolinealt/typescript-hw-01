a7bfc01201713922f8f5c49f647f39245f37032e
Your Name <e.scaroline02@gmail.com>
2024-07-15 20:34:50 +0300

    more details in some objects

--- src/basic/6.ts
+++ src/basic/6.ts
@@ -1,21 +1,21 @@
 interface User {
-    name: string,
-    age: number,
-    email: string,
-    address?: {},
+  name: string;
+  age: number;
+  email: string;
+  address?: { city: string; country: string };
 }
 const poly: User = {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com'
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
 };
 
 const mango: User = {
-    name: 'Mango',
-    age: 30,
-    email: 'john@example.com',
-    address: {
-        city: 'New York',
-        country: 'USA'
-    }
+  name: "Mango",
+  age: 30,
+  email: "john@example.com",
+  address: {
+    city: "New York",
+    country: "USA",
+  },
 };
